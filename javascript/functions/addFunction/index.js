915b054738a1d25f53341e42694403eb820a8cb9
Harunzy <Harunzy55@gmail.com>
2025-01-08 23:02:55 +0000

    add a multiplication function

--- javascript/functions/addFunction/index.js
+++ javascript/functions/addFunction/index.js
@@ -1,12 +1,21 @@
 // write a function that add two different numbers
 
 
-//defining the function
+//defining the function the addition function
 function add(a, b){
     const sum = a + b;
     return sum;
 }
 
-// calling the function
+// calling the addition function
 let newSum = add(23, 34);
-alert(newSum);
+//alert(newSum);
+
+// calling the multiplication function
+function product(c, d){
+    const multiply = c * d;
+    return multiply;
+}
+
+let newMultip = product(5, 10);
+alert(newMultip);
