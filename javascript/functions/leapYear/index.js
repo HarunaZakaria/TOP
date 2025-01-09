637694dae769d93182a7cbb369ede27b32b4ebeb
Harunzy <Harunzy55@gmail.com>
2025-01-09 01:05:44 +0000

    add a new function

---
+++ javascript/functions/leapYear/index.js
@@ -0,0 +1,1 @@
+alert("Hello Leap Year.")
--- javascript/functions/loveCalculator/index.js
+++ javascript/functions/loveCalculator/index.js
@@ -2,5 +2,11 @@ let yourName = prompt("What is your name");
 let partnerName = prompt("What is your partner name");
 
 let loveScore = Math.random() * 100;
-   loveScore = Math.floor(loveScore) + 1;
+
+loveScore = Math.floor(loveScore) + 1;
+
+if(loveScore >= 80) {
+alert( yourName + " You and "+ partnerName +" love score is " + loveScore +"%, your love for each other is like Romeo and Juliet");
+} else{
 alert( yourName + " You and "+ partnerName +" love chances are " + loveScore +"%");
+}
