617230c94291de5a18dcc765fc55c6efe11c0a77
Harunzy <Harunzy55@gmail.com>
2025-01-09 01:56:38 +0000

    add a leap year function

--- javascript/functions/leapYear/index.js
+++ javascript/functions/leapYear/index.js
@@ -1,1 +1,13 @@
-alert("Hello Leap Year.")
+
+
+function leapYear() {
+    const year = prompt("Which year is this? ");
+    if(year % 4 === 0 || year % 100 !== 0 && year % 400 === 0){
+        alert("Leap Year!");
+    } else{
+        alert("Not Leap Year!");
+    }
+    return year;
+}
+
+leapYear();
