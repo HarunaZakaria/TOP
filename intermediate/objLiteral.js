8395cd6fc7edd7dbc8a6c5f7b4dea10ae3c80de0
Sir Harunzy <Harunzy55@gmail.com>
2025-08-12 22:26:27 +0000

    add all

--- intermediate/objLiteral.js
+++ intermediate/objLiteral.js
@@ -12,5 +12,14 @@ const myDotNotation = myObject.property;
 // bracket notation
 const myBraNotation = myObject['otherProperty'];
 
-console.log(myDotNotation); // prints value
-console.log(myBraNotation); // prints 77
+//console.log(myDotNotation); // prints value
+//console.log(myBraNotation); // prints 77
+
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+}
+
+const layer = new Player('Haruna', 'H');
+
+console.log(layer.name);
